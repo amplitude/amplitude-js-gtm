85fa5c001ed3da3e019ef70fbbb23ece3d419a0b
Xinyi Ye <xinyi.ye@amplitude.com>
2024-08-19 11:25:32 -0700

    Update amplitude-wrapper.js

--- src/amplitude-wrapper.js
+++ src/amplitude-wrapper.js
@@ -1,5 +1,3 @@
-import { version } from '../package.json';
-
 /* Amplitude Browser 2.0 SDK begin */
 !function(){"use strict";!function(e,t){var r=e.amplitudeGTM||{_q:[],_iq:{}};if(r.invoked)e.console&&console.error&&console.error("Amplitude snippet has been loaded.");else{var n=function(e,t){e.prototype[t]=function(){return this._q.push({name:t,args:Array.prototype.slice.call(arguments,0)}),this}},s=function(e,t,r){return function(n){e._q.push({name:t,args:Array.prototype.slice.call(r,0),resolve:n})}},o=function(e,t,r){e._q.push({name:t,args:Array.prototype.slice.call(r,0)})},i=function(e,t,r){e[t]=function(){if(r)return{promise:new Promise(s(e,t,Array.prototype.slice.call(arguments)))};o(e,t,Array.prototype.slice.call(arguments))}},a=function(e){for(var t=0;t<g.length;t++)i(e,g[t],!1);for(var r=0;r<y.length;r++)i(e,y[r],!0)};r.invoked=!0;var c=t.createElement("script");c.type="text/javascript",c.integrity="sha384-QQzLtB74EgBbntDs4qfdADKdpgjUX5KWFrvlBU4HdE2xvvAZsXDc9lm7wuXPtOmb",c.crossOrigin="anonymous",c.async=!0,c.src="https://cdn.amplitude.com/libs/analytics-browser-gtm-2.9.3-min.js.gz",c.onload=function(){e.amplitudeGTM.runQueuedFunctions||console.log("[Amplitude] Error: could not load SDK")};var u=t.getElementsByTagName("script")[0];u.parentNode.insertBefore(c,u);for(var l=function(){return this._q=[],this},p=["add","append","clearAll","prepend","set","setOnce","unset","preInsert","postInsert","remove","getUserProperties"],d=0;d<p.length;d++)n(l,p[d]);r.Identify=l;for(var v=function(){return this._q=[],this},f=["getEventProperties","setProductId","setQuantity","setPrice","setRevenue","setRevenueType","setEventProperties"],m=0;m<f.length;m++)n(v,f[m]);r.Revenue=v;var g=["getDeviceId","setDeviceId","getSessionId","setSessionId","getUserId","setUserId","setOptOut","setTransport","reset","extendSession"],y=["init","add","remove","track","logEvent","identify","groupIdentify","setGroup","revenue","flush"];a(r),r.createInstance=function(e){return r._iq[e]={_q:[]},a(r._iq[e]),r._iq[e]},e.amplitudeGTM=r,e.amplitude||(e.amplitude=e.amplitudeGTM)}}(window,document)}();
 /* Amplitude Browser 2.0 SDK end */
@@ -145,15 +143,29 @@ var amplitudeUserAgentEnrichmentPlugin=function(i){"use strict";var e=function()
           type: 'enrichment',
           setup: async () => undefined,
           execute: async (event) => {
-              event['library'] = `amplitude-ts-gtm/${version}`;
+              event['library'] = `amplitude-ts-gtm/3.9.0`;
               return event;
           },
       };
   };
 
   var init = function(client, args) {
+
       const argsLength = args.length;
       const configuration = args[argsLength - 1];
+
+      const excludeReferrers = [];
+      if (configuration.defaultTracking.attribution.excludeReferrersText){
+          excludeReferrers.push(...configuration.defaultTracking.attribution.excludeReferrersText);
+          delete configuration.defaultTracking.attribution.excludeReferrersText;
+      }
+      if (configuration.defaultTracking.attribution.excludeReferrersRegex){
+          const excludeReferrersRegex = configuration.defaultTracking.attribution.excludeReferrersRegex.map(item => new RegExp(item));
+          excludeReferrers.push(...excludeReferrersRegex);
+          delete configuration.defaultTracking.attribution.excludeReferrersRegex;
+      }
+      configuration.defaultTracking.attribution.excludeReferrers = excludeReferrers;
+
       const userAgentEnrichmentOptions = configuration['userAgentEnrichmentOptions'];
       const pageViewLegacy = configuration['pageViewLegacy'];
 
